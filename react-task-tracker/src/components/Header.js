6de520f832245d67774e8b60caad2a00607aee4b
MikeNotu <mikebateman98@gmail.com>
2021-05-09 02:08:22 -0500

    Styling part 2

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,20 +1,24 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types"
 
-
-const Header = ({title}) => {
-    return (
-        <header>
-            <h1 style={{color:'red',backgroundColor:'black'}}>{title}</h1>
-        </header>
-    )
+const Header = ({ title }) => {
+  return (
+    <header>
+      <h1 style={headingStyle}>{title}</h1>
+    </header>
+  )
 }
 
 Header.defaultProps = {
-    title: 'Task Tracker',
+  title: "Task Tracker",
 }
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+}
+
+const headingStyle = {
+  color: "red",
+  backgroundColor: "black",
 }
 
 export default Header
