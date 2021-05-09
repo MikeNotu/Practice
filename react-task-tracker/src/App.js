e6f60a1e407f17a3455939b9c11c5a2c55b9a401
MikeNotu <mikebateman98@gmail.com>
2021-05-09 01:45:14 -0500

    Classes

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,38 +1,23 @@
-function App() {
+import React from 'react'
 
-const name = 'Mike'
-var x = true
+import Header from './components/Header'
 
-let array = ["1"]
-
-array.forEach(element => {
-  
-console.log('xd ')
-});
-
-if(x===true){
-  console.log('Hola')
-}else{
-  console.log(':(')
-}
-
-  return (
-    <div className="container">
-      <h1>Hello from React</h1>
-
-      <h2>Hello {name}</h2>
-      <h2>1+1</h2>
-      <h2>{1+1}</h2>
-
-      <h2>{x ? 'Yes' : 'No'}</h2>
-
-
-
-    </div>
+// function App() {
+//   return (
+//     <div className="container">
+//       <Header />
+//     </div>
     
 
     
-  );
+//   );
+// }
+
+class App extends React.Component{
+  render(){
+    return<h1>Hello from a class</h1>
+  }
 }
+
 
 export default App;
