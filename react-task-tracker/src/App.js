08cb1ec5df283cab7b447d13f5587bd64b0432d1
MikeNotu <mikebateman98@gmail.com>
2021-05-09 00:08:03 -0500

    JSX arrays & conditionals testing

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+const name = 'Mike'
+var x = true
+
+let array = ["1"]
+
+array.forEach(element => {
+  
+console.log('xd ')
+});
+
+if(x===true){
+  console.log('Hola')
+}else{
+  console.log(':(')
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello from React</h1>
+
+      <h2>Hello {name}</h2>
+      <h2>1+1</h2>
+      <h2>{1+1}</h2>
+
+      <h2>{x ? 'Yes' : 'No'}</h2>
+
+
+
     </div>
+    
+
+    
   );
 }
 
