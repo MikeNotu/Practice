f662c5702bb5fad45318eae0b766999ebed6719e
MikeNotu <mikebateman98@gmail.com>
2021-05-12 00:11:25 -0500

    Lorem

--- React_Practice/react-cards-bootstrap/src/components/Card.js
+++ React_Practice/react-cards-bootstrap/src/components/Card.js
@@ -1,7 +1,22 @@
 import React from "react"
 
 function Card() {
-  return <div>Card Component</div>
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h4 className="card-title">My Title</h4>
+        <p className="card-text text-secondary">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod
+          tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex
+          ea commodi consequat. Quis aute iure reprehenderit in voluptate velit
+          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat
+          cupiditat non proident, sunt in culpa qui officia deserunt mollit anim
+          id est laborum.
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Card
