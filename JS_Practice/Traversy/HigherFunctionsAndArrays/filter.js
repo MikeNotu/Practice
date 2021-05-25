0b8a5a3c50ad3699739cd1a826aedac2d9f3c8b5
MikeNotu <mikebateman98@gmail.com>
2021-05-25 12:01:11 -0500

    Filer JS

--- JS_Practice/Traversy/HigherFunctionsAndArrays/filter.js
+++ JS_Practice/Traversy/HigherFunctionsAndArrays/filter.js
@@ -12,3 +12,28 @@ const companies = [
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
+/*let canDrink = [];
+for(let i = 0; i < ages.length; i++){
+	if(ages[i] >= 21){
+		canDrink.push(ages[i]);
+	}
+}*/
+
+// const canDrink = ages.filter(function(age){
+// 	if(age >=21){
+// 		return true;
+// 	}
+// });
+
+// const canDrink = ages.filter(age=> age>=21);
+
+// console.log(canDrink);
+
+
+const retailCompanies = companies.filter(function(company){
+	if(company.category === 'Retail'){
+		return true;
+	}
+});
+
+console.log(retailCompanies);
