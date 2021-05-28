6ab18875de7267614b50dd0d162fdf7b615e9106
MikeNotu <mikebateman98@gmail.com>
2021-05-28 18:41:11 -0500

    js using another js file

---
+++ JS_Practice/jsUsingAnotherJsFile.js
@@ -0,0 +1,11 @@
+// javascript using another js file
+
+
+// module.js
+export function hello() {
+  return "Hello";
+}
+
+// main.js
+import {hello} from 'module'; // or './module'
+let val = hello(); // val is "Hello";
