eabbcedf691a5c63cf59553103f0759a553c078c
MikeNotu <mikebateman98@gmail.com>
2021-07-20 02:11:22 -0500

    Php Variables

---
+++ Php_Practice/Syntax/04.php
@@ -0,0 +1,8 @@
+<!-- Insert the correct characters to write a multi-line comment. -->
+
+
+/*
+ This is a
+multi-line
+comment
+*/
---
+++ Php_Practice/Syntax/05.php
@@ -0,0 +1,4 @@
+<!-- Statements in PHP have to end with a special character, which one? -->
+
+
+echo "Hello World";
